279250be7965314ae656e5918747bf7ee11f6e58
Jose Jaime Felix Garcia <josejaimefelixgarcia@gmail.com>
2026-01-18 22:22:51 +0800

    feat: RAG Implementation on Dialogue Agent

--- town_hall_agents/dialogue_agent.py
+++ town_hall_agents/dialogue_agent.py
@@ -1,13 +1,21 @@
+import os
 from agents import Agent
 from town_hall_agents.triage_agent import triage_agent
+from agents import FileSearchTool
 
 dialogue_agent_instructions = """
 You are Dialogue Agent. You will be the agent directly conversing with the user.
 Handover the user's message to the Triage Agent.
 """
 
+file_search = FileSearchTool(
+   max_num_results=3,
+   vector_store_ids=[os.getenv("VECTOR_STORE_ID")]
+)
+
 dialogue_agent=Agent(
    name="Dialogue Agent",
    instructions=dialogue_agent_instructions,
-   handoffs=[triage_agent]
+   handoffs=[triage_agent],
+   tools=[file_search]
 )
